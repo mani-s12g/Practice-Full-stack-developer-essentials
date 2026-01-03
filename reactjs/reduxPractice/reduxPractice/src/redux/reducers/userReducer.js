9a4798a7217089fd9a46c191fc6c24dafc880dd0
mani.s12g@gmail.com <mani.s12g@gmail.com>
2026-01-03 15:38:34 +0530

    redux ...

--- reactjs/reduxPractice/reduxPractice/src/redux/reducers/userReducer.js
+++ reactjs/reduxPractice/reduxPractice/src/redux/reducers/userReducer.js
@@ -1,4 +1,4 @@
-import {FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_FAILURE} from "../actions/types"
+import { FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_FAILURE } from "../actions/types"
 
 const initialState = {
     loading: false,
